3d297c9185cdbd372d431acb54a1ebb55952e8d1
yaocs <yao.cs@qq.com>
2016-12-07 16:06:13 +0800

    update 20161207

--- dist/js/global.js
+++ dist/js/global.js
@@ -13,21 +13,38 @@ $(function () {
       "pageLength": 50
     }
   });
+
   // tooltip
   $("[data-toggle='tooltip']").tooltip();
+
   // 更多内容 panel
-  $('.tip').hover(
-    function () {
-      var cur_panel = $(this).find('.tipContent');
-      var h = cur_panel.height();
-      var w1 = cur_panel.width();
-      var w2 = $(this).width();
-      var xpos = Math.abs(Math.ceil((w2 - w1) / 2));
-      cur_panel.css({
-        top: -(h - 4) + 'px',
-        left: xpos + 'px'
-      })
-      $(this).find('.tipContent').fadeToggle();
-    }
-  )
+  $('.tip').hover(function () {
+    var cur_panel = $(this).find('.tipContent');
+    var h = cur_panel.height();
+    var w1 = cur_panel.width();
+    var w2 = $(this).width();
+    var xpos = Math.abs(Math.ceil((w2 - w1) / 2));
+    cur_panel.css({
+      top: -(h - 4) + 'px',
+      left: xpos + 'px'
+    })
+    $(this).find('.tipContent').stop().fadeToggle();
+  });
+
+  // 删除数据
+  $('body').on('click', '.delete', function () {
+    var curid = $(this).parents('tr').attr('data-id');
+    $('#deleteConfirmBtn').attr('data-curid', curid);
+  });
+
+  // 删除确认
+  $('#deleteConfirmBtn').on('click', function () {
+    var id = $(this).attr('data-curid');
+    deleteConfirm(id);
+  });
+
 });
+
+function deleteConfirm(id) {
+  $('.dataList').find('tr[data-id=' + id + ']').remove();
+}
